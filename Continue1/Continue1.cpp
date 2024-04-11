f55565b9ad8c4d8c03296d73a0dd55c438d1d01d
Diego Armando Sevilla Cuevas <diegosevilla29112003@gmail.com>
2024-04-11 14:20:44 -0700

    Se añadieron comentarios y sistematizacion del proyecto

--- Continue1/Continue1.cpp
+++ Continue1/Continue1.cpp
@@ -6,19 +6,19 @@
 using namespace std;
 
 // Enumeración para representar direcciones
-enum class Direction { LEFT, RIGHT, UP, DOWN };
+enum class Direction { LEFT, RIGHT, UP, DOWN }; // Direcciones posibles
 
-// Función para obtener un número aleatorio en un rango
+// Función para generar un número entero aleatorio dentro del rango [min, max]
 int getRandomInt(int min, int max) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(min, max);
-    return distr(gen);
+    random_device rd; // Crear un objeto random_device para generar semillas aleatorias
+    mt19937 gen(rd()); // Crear un generador de números pseudoaleatorios con la semilla generada por random_device
+    uniform_int_distribution<> distr(min, max); // Crear una distribución uniforme de números enteros en el rango [min, max]
+    return distr(gen); // Generar y devolver un número entero aleatorio utilizando la distribución uniforme y el generador
 }
 
 // Función para obtener un carácter aleatorio de un conjunto dado
 char getRandomChar(const vector<char>& characters) {
-    int randomIndex = getRandomInt(0, characters.size() - 1);
+    int randomIndex = getRandomInt(0, characters.size() - 1); // crea un numero aleatorio entre 0 y la longitud del vector
     return characters[randomIndex];
 }
 
@@ -26,7 +26,7 @@ char getRandomChar(const vector<char>& characters) {
 vector<vector<string>> createMap(const vector<int>& mapSize, const string& wallSymbol) {
     vector<vector<string>> map;
     for (int x = 0; x < mapSize[0]; x++) {
-        vector<string> row(mapSize[1], wallSymbol);
+        vector<string> row(mapSize[1], wallSymbol); // Se crea un vector del tamaño x y se rellena con el simbolo de pared
         map.push_back(row);
     }
     return map;
@@ -55,7 +55,7 @@ bool isNextMoveWall(const vector<vector<string>>& map, const vector<int>& player
     int y = playerPosition[1];
     switch (direction) {
     case Direction::LEFT:
-        return (x > 0 && map[y][x - 1] == wallSymbol);
+        return (x > 0 && map[y][x - 1] == wallSymbol); // Comprueba si estas en el borde o si es un simbolo lo mismo en las demas
     case Direction::RIGHT:
         return (x < map.size() - 1 && map[y][x + 1] == wallSymbol);
     case Direction::UP:
@@ -68,30 +68,44 @@ bool isNextMoveWall(const vector<vector<string>>& map, const vector<int>& player
 }
 
 // Función para colocar una zona de movimiento en una posición aleatoria
-void spawnMoveZone(vector<vector<string>>& map, const vector<int>& playerPosition, const string& routeSymbol, const string& wallSymbol) {
-    char key = getRandomChar({ 'a', 'w', 's', 'd' });
+void spawnMoveZone(vector<vector<string>>& map, vector<int> mapSize, const vector<int>& playerPosition, const string& routeSymbol, const string& wallSymbol) {
+    char key;
+    vector<char> validMovements = { 'a', 'w', 's', 'd' };
     int x = playerPosition[0];
     int y = playerPosition[1];
+    if (x == 0) { // Si x igual a 0 eliminamos el que se pueda posicionar a la izquierda
+        validMovements.erase(remove(validMovements.begin(), validMovements.end(), 'a'), validMovements.end()); 
+    } 
+    else if (x == mapSize[1] - 1) { // Si x igual al mapSize -1 eliminamos el que se pueda posicionar a la izquierda
+        validMovements.erase(remove(validMovements.begin(), validMovements.end(), 'd'), validMovements.end());
+    }
+    if (y == 0) { // Si y es igual a 0 eliminamos el que se pueda posicionar hacia arriba
+        validMovements.erase(remove(validMovements.begin(), validMovements.end(), 'w'), validMovements.end());
+    }
+    else if (y == mapSize[0] - 1) { // Si y es igual a mapSize -1 eliminamos el que se pueda posicionar hacia abajo
+        validMovements.erase(remove(validMovements.begin(), validMovements.end(), 's'), validMovements.end());
+    }
+    key = getRandomChar(validMovements); // Se llama a la funcion para que torne un character random despues de a ver realizado las comprobaciones
 
     switch (tolower(key)) {
     case 'a':
         if (x > 0) {
-            map[y][x - 1] = routeSymbol;
+            modifyMapIndex(map, { x - 1, y }, routeSymbol); // Se resta uno a x para que se mueva a la derecha la misma logica para el resto
         }
         break;
     case 'd':
         if (x < map.size() - 1) {
-            map[y][x + 1] = routeSymbol;
+            modifyMapIndex(map, { x + 1, y }, routeSymbol);
         }
         break;
     case 'w':
         if (y > 0) {
-            map[y - 1][x] = routeSymbol;
+            modifyMapIndex(map, { x, y - 1}, routeSymbol);
         }
         break;
     case 's':
         if (y < map[0].size() - 1) {
-            map[y + 1][x] = routeSymbol;
+            modifyMapIndex(map, { x, y + 1}, routeSymbol);
         }
         break;
     default:
@@ -101,24 +115,24 @@ void spawnMoveZone(vector<vector<string>>& map, const vector<int>& playerPositio
 
 
 // Función para mover al jugador en una dirección dada
-void movePlayer(vector<vector<string>>& map, vector<int>& playerPosition, const string& playerSymbol, const string& routeSymbol, const string& wallSymbol, Direction direction) {
+void movePlayer(vector<vector<string>>& map, vector<int> mapSize, vector<int>& playerPosition, const string& playerSymbol, const string& routeSymbol, const string& wallSymbol, Direction direction) {
     int x = playerPosition[0];
     int y = playerPosition[1];
     switch (direction) {
-    case Direction::LEFT:
-        if (x > 0 && isNextMoveWall(map, playerPosition, wallSymbol, Direction::LEFT) == false) {
-            modifyMapIndex(map, playerPosition, wallSymbol);
-            playerPosition[0] -= 1;
-            modifyMapIndex(map, playerPosition, playerSymbol);
-            spawnMoveZone(map, playerPosition, routeSymbol, wallSymbol);
-        }
+    case Direction::LEFT: 
+        if (x > 0 && isNextMoveWall(map, playerPosition, wallSymbol, Direction::LEFT) == false) { // Se comprueban los bordes y las colisiones
+            modifyMapIndex(map, playerPosition, wallSymbol); // La posicion actual se remplaza por un simbolo de pared
+            playerPosition[0] -= 1; // se modifica la posicion actual del jugador para que este uno mas a la derecha
+            modifyMapIndex(map, playerPosition, playerSymbol); // se actualiza la posicion del jugador
+            spawnMoveZone(map, mapSize, playerPosition, routeSymbol, wallSymbol); // se spawnea una nueva zona de movimiento
+        } // Misma logica aplica al resto
         break;
     case Direction::RIGHT:
         if (x < map.size() - 1 && isNextMoveWall(map, playerPosition, wallSymbol, Direction::RIGHT) == false) {
             modifyMapIndex(map, playerPosition, wallSymbol);
             playerPosition[0] += 1;
             modifyMapIndex(map, playerPosition, playerSymbol);
-            spawnMoveZone(map, playerPosition, routeSymbol, wallSymbol);
+            spawnMoveZone(map, mapSize, playerPosition, routeSymbol, wallSymbol);
         }
         break;
     case Direction::UP:
@@ -126,7 +140,7 @@ void movePlayer(vector<vector<string>>& map, vector<int>& playerPosition, const 
             modifyMapIndex(map, playerPosition, wallSymbol);
             playerPosition[1] -= 1;
             modifyMapIndex(map, playerPosition, playerSymbol);
-            spawnMoveZone(map, playerPosition, routeSymbol, wallSymbol);
+            spawnMoveZone(map, mapSize, playerPosition, routeSymbol, wallSymbol);
         }
         break;
     case Direction::DOWN:
@@ -134,7 +148,7 @@ void movePlayer(vector<vector<string>>& map, vector<int>& playerPosition, const 
             modifyMapIndex(map, playerPosition, wallSymbol);
             playerPosition[1] += 1;
             modifyMapIndex(map, playerPosition, playerSymbol);
-            spawnMoveZone(map, playerPosition, routeSymbol, wallSymbol);
+            spawnMoveZone(map, mapSize, playerPosition, routeSymbol, wallSymbol);
         }
         break;
     default:
@@ -149,14 +163,14 @@ int main() {
     string wallSymbol = "#";
     string routeSymbol = " ";
 
-    vector<int> mapSize{ 10 , 10 };
-    vector<vector<string>> map = createMap(mapSize, wallSymbol);
+    vector<int> mapSize{ 10 , 10 }; // Tamaño del mapa
+    vector<vector<string>> map = createMap(mapSize, wallSymbol); // Se crea el mapa
 
-    vector<int> playerPosition{ getRandomInt(1, 8), getRandomInt(1, 8) };
+    vector<int> playerPosition{ getRandomInt(0, mapSize[1] - 1), getRandomInt(0, mapSize[0] - 1) }; // La posicion del jugador es aleatoria  y evita q
 
     modifyMapIndex(map, playerPosition, playerSymbol);
 
-    spawnMoveZone(map, playerPosition, routeSymbol, wallSymbol);
+    spawnMoveZone(map, mapSize, playerPosition, routeSymbol, wallSymbol);
 
     while (true) {
         system("cls");
@@ -182,8 +196,56 @@ int main() {
             continue; // Ignorar entradas no válidas
         }
 
-        movePlayer(map, playerPosition, playerSymbol, routeSymbol, wallSymbol, direction);
+        movePlayer(map, mapSize, playerPosition, playerSymbol, routeSymbol, wallSymbol, direction);
     }
 
     return 0;
 }
+
+// Tarea:
+// Nombre:
+// Solucion:
+
+// Tarea: Evitar que el jugador se quede sin movimientos al spawnear un moveZone fuera de los limites del mapa 
+// Nombre: Diego
+// Solucion: Se comprueban los bordes del mapa antes de retornar el randomCharacter
+
+
+// Tarea: Crear colisiones
+// Nombre: Diego
+// Solucion: Se agregaron comprobaciones de limites de coordenadas antes de que se ejecuten los movimientos del jugador
+
+
+
+// Tarea: Crear rutas de movimiento aleatorias
+// Nombre: Diego
+// Solucion: se creo la funcion spawnMoveZone la cual selecciona una direccion aleatoria entorno a las coordenadas del jugador y modifica 
+// dicha direccion poniendo un simbolo de ruta, tambien se creo la funcion getRandomChar que recibe un vector de char y retorne un valor aleatorio
+
+
+
+// Tarea: Crear una funcion que permita que el jugador se mueva
+// Nombre: Diego
+// Solucion: Se creo un enum class Direction que corresponde a las direcciones de movimiento
+// En el codigo se agrego un bucle el cual recibe las pulsaciones del teclado por consola y estas pulsaciones corresponden a una direccion
+// se creo la funcion movePlayer la cual recibe lo necesario y dependiendo de la direccion dada llama a modifyMapIndex e inserta el symbolo
+// del jugador en la coordenada modificada con el movimiento
+
+
+// Tarea: Crear una funcion que permita insertar un symbolo en unas coordenadas dadas en el mapa
+// Nombre: Diego
+// Solucion: Se creo la funcion recibe, el mapa, el symbolo, y las coordenadas
+
+// Tarea: Crear una funcion que retornde un int dado un rango
+// Nombre: Diego
+// Solucion: Se autoexplica
+
+
+// Tarea: Imprimir mapa
+// Nombre: Diego
+// Solucion: Se creo una funcion que recibe el mapa y lo imprime
+
+
+// Tarea: Crear mapa de vector por vector de string de n por n
+// Nombre: Diego
+// Solucion: Se creo una funcion que recibe un vector<int> de 2x2, y un string que corresponde a las paredes, retorna un mapa de NxN en relacion al vector dado
